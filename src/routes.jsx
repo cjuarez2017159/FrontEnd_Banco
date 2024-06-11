013f980c4719fbf13ae1a076420b5be066fa4d64
arodriguez2019473 <arodriguez-2019473@kinal.edu.gt>
2024-06-11 17:14:41 -0600

    se hace cambios en router

--- src/routes.jsx
+++ src/routes.jsx
@@ -1,7 +1,10 @@
 import { DashboardPage } from "./pages/dashboard";
 import { AuthPage } from "./pages/auth";
+import { Main_guest } from "./pages/Main_guest";
+
 
 const routes = [
+    { path: '/', element: <Main_guest /> },
     {path: '/auth', element: <AuthPage/>},
     {path: "/", element: <DashboardPage/>}
 ]
